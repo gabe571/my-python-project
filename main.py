4eed7174bd6c881fb2510248f1bd0c19acc46045
Gabe571 <gabriel.castro1028@gmail.com>
2021-11-09 18:48:07 -0800

    scope check

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ name_of_unit = 'seconds'
 print(f'20 days are {20 * calculation_to_units}  {name_of_unit}')
 
 
-def days_to_units(num_of_days):
+def days_to_units(num_of_days, custom_message):
     print(f'{num_of_days} days are {num_of_days * calculation_to_units}  {name_of_unit}')
+    print(custom_message)
 
-days_to_units(65)
-days_to_units(35)
-days_to_units(50)
+def scope_check():
+    local_var = 'my local var'
+    print(name_of_unit)
+    print(local_var)
+
+scope_check()
+
+days_to_units(65, 'testing')
