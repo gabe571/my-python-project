84adb36777fbce92dbf684005c3348c36176cfde
Gabe571 <gabriel.castro1028@gmail.com>
2021-11-09 18:37:28 -0800

    function params, inputs

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ name_of_unit = 'seconds'
 print(f'20 days are {20 * calculation_to_units}  {name_of_unit}')
 
 
-def days_to_units():
-    print(f'20 days are {20 * calculation_to_units}  {name_of_unit}')
+def days_to_units(num_of_days):
+    print(f'{num_of_days} days are {num_of_days * calculation_to_units}  {name_of_unit}')
 
-days_to_units()
+days_to_units(65)
+days_to_units(35)
+days_to_units(50)
