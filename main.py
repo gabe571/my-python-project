9d9b2e9c93449241ffdd50023c44e3b58abde9fd
Gabe571 <gabriel.castro1028@gmail.com>
2021-11-09 18:28:38 -0800

    testing

---
+++ main.py
@@ -0,0 +1,10 @@
+calculation_to_units = 24 * 60 * 60
+name_of_unit = 'seconds'
+
+print(f'20 days are {20 * calculation_to_units}  {name_of_unit}')
+
+
+def days_to_units():
+    print(f'20 days are {20 * calculation_to_units}  {name_of_unit}')
+
+days_to_units()
