6e38d3d583b2e8167c6dc17a5ead732003530d81
Gabe571 <gabriel.castro1028@gmail.com>
2021-11-15 17:51:39 -0800

    input() validation clean up

--- main.py
+++ main.py
@@ -9,17 +9,18 @@ def days_to_units(num_of_days):
         return (f'{num_of_days} days are {num_of_days * calculation_to_units}  {name_of_unit}')
     elif num_of_days == 0:
         return ('please enter number greater then zero')
+
+def validate_and_execute():
+    if user_input.isdigit():
+        user_input_number = int(user_input)
+        cal_value = days_to_units(user_input_number)
+        print(cal_value)
     else:
-        return ('you entered a negative number, please enter a positive number')
+        print('not a valid number, please try again')
 
 user_input = input('enter a number of days\n')
+validate_and_execute()
 
-if user_input.isdigit():
-    user_input_number = int(user_input)
-    cal_value = days_to_units(user_input_number)
-    print(cal_value)
-else:
-    print('not a valid number, please try again')
 #   print(custom_message)
 
 
