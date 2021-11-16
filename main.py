49b5369b238afd56e3cd9c3a4cf56e6c330b9ff9
Gabe571 <gabriel.castro1028@gmail.com>
2021-11-15 17:35:26 -0800

    input() validation

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ name_of_unit = 'seconds'
 def days_to_units(num_of_days, custom_message):
     print(f'{num_of_days} days are {num_of_days * calculation_to_units}  {name_of_unit}')
 
-
 user_input = input('enter a number of days\n')
 print(user_input)
 #   print(custom_message)
