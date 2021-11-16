1c3004666295cac174e3d3faa485eb0190e700c7
Gabe571 <gabriel.castro1028@gmail.com>
2021-11-15 17:06:19 -0800

    input()

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 calculation_to_units = 24 * 60 * 60
 name_of_unit = 'seconds'
 
-print(f'20 days are {20 * calculation_to_units}  {name_of_unit}')
+#print(f'20 days are {20 * calculation_to_units}  {name_of_unit}')
 
 
 def days_to_units(num_of_days, custom_message):
     print(f'{num_of_days} days are {num_of_days * calculation_to_units}  {name_of_unit}')
-    print(custom_message)
 
-def scope_check():
-    local_var = 'my local var'
-    print(name_of_unit)
-    print(local_var)
 
-scope_check()
+user_input = input('enter a number of days\n')
+print(user_input)
+#   print(custom_message)
 
-days_to_units(65, 'testing')
+
+# def scope_check():
+#    local_var = 'my local var'
+#    print(name_of_unit)
+#    print(local_var)
+
+# scope_check()
+
+# days_to_units(65, 'testing')
