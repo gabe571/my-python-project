c82869077aa40a1cded9f9e9b110c5c06de4fd28
Gabe571 <gabriel.castro1028@gmail.com>
2021-11-15 17:15:46 -0800

    input() return value

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ name_of_unit = 'seconds'
 #print(f'20 days are {20 * calculation_to_units}  {name_of_unit}')
 
 
-def days_to_units(num_of_days, custom_message):
-    print(f'{num_of_days} days are {num_of_days * calculation_to_units}  {name_of_unit}')
-
+def days_to_units(num_of_days):
+    return (f'{num_of_days} days are {num_of_days * calculation_to_units}  {name_of_unit}')
 
 user_input = input('enter a number of days\n')
-print(user_input)
+user_input_number = int(user_input)
+cal_value = days_to_units(user_input_number)
+print(cal_value)
+
 #   print(custom_message)
 
 
